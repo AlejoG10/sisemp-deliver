a0f29a1dd50ee88a0cdcf3587b1c29ecd4aba09d
Alejandro <alejogonza99@hotmail.com>
2023-05-20 11:39:04 -0500

    Change banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-import banner from "../images/banner.png";
+// import banner from "../images/banner.png";
+import banner from "../images/banner.jpeg";
 
 export default function Banner() {
   return (
-    <div className="mb-4 w-full max-w-[1620px] h-[400px]">
+    <div className="mb-4 w-full max-w-[1620px] h-[450px]">
       <img src={banner} alt="banner" className="w-full h-full object-cover" />
     </div>
   );
